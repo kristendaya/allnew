92a7d3a9d3da1c2e16cb551ff70928f8e9171c84
kristendaya <126866834+kristendaya@users.noreply.github.com>
2023-03-17 08:58:31 +0900

    .

---
+++ c/gcc/lib/calc.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include "libmy.h"
+
+void main() {
+
+int x,y ;
+printf("input two numbers :");
+scanf("%d %d", &x, &y);
+pirntf("%d + %d = %d\n",x,y,plus(x,y));
+printf("%d - %d = %d\n" x,y,minus(x,y));
+}
