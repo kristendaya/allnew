cc544be77f74a66a092499d701636d3e2c3867eb
kristendaya <clare0339@gmail.com>
2023-04-10 10:01:11 +0900

    temp

--- node/atlas/atlas.js
+++ node/atlas/atlas.js
@@ -1,27 +1,29 @@
 var axios = require('axios');
+
+const env = require('dotenv').config({ path: "../../.env" });
+
 var data = JSON.stringify({
     "collection": "testdb",
     "database": "test",
     "dataSource": "Cluster0",
     "projection": {
         "_id": 0,
-        "id":1,
-        "name" :1
-
+        "id": 1,
+        "name": 1
     }
 });
-            
+
 var config = {
     method: 'post',
-    url: 'https://us-west-2.aws.data.mongodb-api.com/app/data-rgnpg/endpoint/data/v1/action/findOne',
+    url: 'https://us-west-2.aws.data.mongodb-api.com/app/data-bydgv/endpoint/data/v1/action/findOne',
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Request-Headers': '*',
-
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': process.env.ATLAS_API
     },
     data: data
 };
-            
+
 axios(config)
     .then(function (response) {
         console.log(JSON.stringify(response.data));
